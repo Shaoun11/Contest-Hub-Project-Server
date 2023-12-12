81539eed53536ebfa112a549fbdc2b5bc3429e33
shaoun-01 <heyshaoun@gmail.com>
2023-12-13 03:45:47 +0600

    first commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     const database = client.db("contestHub").collection("contest");
     const userdatabase = client.db("contestHub").collection("users");
-    await client.connect();
+ 
 
     //get contest data
     app.get("/allcontest", async (req, res) => {
